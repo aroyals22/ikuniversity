d040c35c9ed63d25563bf0bd3dfbd06d66f36df6
Anthony Arroyo <anthony.aarroy2@gmail.com>
2025-10-06 14:03:28 -0500

    edit authconfit file

--- auth.config.js
+++ auth.config.js
@@ -2,5 +2,14 @@ export const authConfig = {
 	session: {
 		strategy: 'jwt',
 	},
+	pages: {
+		signIn: '/login',
+	},
+	callbacks: {
+		authorized({ auth, request: { nextUrl } }) {
+			const isLoggedIn = !!auth?.user;
+			return isLoggedIn;
+		},
+	},
 	providers: [],
 };

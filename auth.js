1faf9e830a823ebb5dbf00a3585f08b9f0e9125e
Anthony Arroyo <anthony.aarroy2@gmail.com>
2025-10-06 14:49:50 -0500

    edit authconfit file

--- auth.js
+++ auth.js
@@ -14,30 +14,28 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 				try {
 					const user = await User.findOne({ email: credentials?.email });
 
-					if (user) {
-						const isMatch = await bcrypt.compare(
-							credentials.password,
-							user.password
-						);
-
-						if (isMatch) {
-							return {
-								id: user._id.toString(),
-								email: user.email,
-								name: user.firstName + ' ' + user.lastName,
-								role: user.role,
-							};
-						} else {
-							console.error('Password Mismatch');
-							throw new Error('Check your password');
-						}
-					} else {
-						console.error('User not found');
-						throw new Error('User not found');
+					if (!user) {
+						return null;
 					}
+
+					const isMatch = await bcrypt.compare(
+						credentials.password,
+						user.password
+					);
+
+					if (isMatch) {
+						return {
+							id: user._id.toString(),
+							email: user.email,
+							name: user.firstName + ' ' + user.lastName,
+							role: user.role,
+						};
+					}
+
+					return null;
 				} catch (err) {
-					console.error(err);
-					throw new Error(err);
+					console.error('Auth error:', err);
+					return null;
 				}
 			},
 		}),

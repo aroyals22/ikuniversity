d43b897f48e228629d9ebcabf4f62759e1f09fe6
Anthony Arroyo <anthony.aarroy2@gmail.com>
2025-10-19 18:28:06 -0500

    remove debug box

--- app/dashboard/quiz-sets/page.jsx
+++ app/dashboard/quiz-sets/page.jsx
@@ -16,19 +16,6 @@ const QuizSets = async () => {
 	
 	return (
 		<div className='p-6'>
-			{/* TEMPORARY DEBUG */}
-			<div className='mb-4 p-4 bg-yellow-100 border-2 border-yellow-600 rounded text-sm'>
-				<p className='font-bold text-lg mb-2'>🐛 Debug Info:</p>
-				<p className='mb-2'>
-					<strong>Total quiz sets from DB:</strong> {quizSetsall.length}
-				</p>
-				<div className='bg-white p-2 rounded border border-gray-300 overflow-auto max-h-60'>
-					<pre className='text-xs'>
-						{JSON.stringify(mappedQuizSets, null, 2)}
-					</pre>
-				</div>
-			</div>
-
 			<DataTable columns={columns} data={mappedQuizSets} />
 		</div>
 	);

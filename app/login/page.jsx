7ffbb07f00ed26c5f7b6266e5d903db017fa06ec
Anthony Arroyo <anthony.aarroy2@gmail.com>
2025-10-24 22:43:39 -0500

    added header to login and register page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,12 +1,26 @@
 import { LoginForm } from "./_components/login-form";
+import MainNav from '@/components/MainNav';
+
+const navItems = [
+	{ title: 'Courses', href: '/courses' },
+	{ title: 'Testimonials', href: '/#testimonials' },
+	{ title: 'Contact', href: '/contact' },
+];
 
 const LoginPage = () => {
   return (
-    <div className="w-full flex-col h-screen flex items-center justify-center">
-      <div className="container">
-        <LoginForm />
-      </div>
-    </div>
-  );
+		<>
+			<header className='container z-40 bg-white'>
+				<div className='flex h-20 items-center justify-between py-6'>
+					<MainNav items={navItems} />
+				</div>
+			</header>
+			<div className='w-full flex-col h-[calc(100vh-80px)] flex items-center justify-center'>
+				<div className='container'>
+					<LoginForm />
+				</div>
+			</div>
+		</>
+	);
 };
 export default LoginPage;
--- app/register/[role]/page.jsx
+++ app/register/[role]/page.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { redirect } from 'next/navigation';
 import { SignupForm } from '../_components/signup-form';
+import MainNav from '@/components/MainNav';
 
 const VALID_ROLES = ['student', 'instructor'];
+
+const navItems = [
+	{ title: 'Courses', href: '/courses' },
+	{ title: 'Testimonials', href: '/#testimonials' },
+	{ title: 'Contact', href: '/contact' },
+];
 
 const RegisterPage = async ({ params }) => {
 	const { role } = await params;
@@ -13,11 +20,18 @@ const RegisterPage = async ({ params }) => {
 	}
 
 	return (
-		<div className='w-full h-screen flex flex-col items-center justify-center'>
-			<div className='container'>
-				<SignupForm role={role} />
+		<>
+			<header className='container z-40 bg-white'>
+				<div className='flex h-20 items-center justify-between py-6'>
+					<MainNav items={navItems} />
+				</div>
+			</header>
+			<div className='w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center'>
+				<div className='container'>
+					<SignupForm role={role} />
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 

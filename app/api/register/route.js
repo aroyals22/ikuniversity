41d2616eb7cbd2ee7dd9ff25454123bc5de11c82
Anthony Arroyo <anthony.aarroy2@gmail.com>
2025-10-26 16:01:57 -0500

    added welcome email upon registration

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -2,6 +2,8 @@ import { dbConnect } from '@/service/mongo';
 import { NextRequest, NextResponse } from 'next/server';
 import { hashPassword } from '@/auth';
 import { User } from '@/model/user-model';
+import { sendEmails } from '@/lib/emails';
+import React from 'react';
 
 export const POST = async (request) => {
 	await dbConnect();
@@ -47,6 +49,69 @@ export const POST = async (request) => {
 
 	try {
 		await User.create(newUser);
+
+		// Send welcome email
+		try {
+			const loginLink = `${process.env.NEXT_PUBLIC_BASE_URL}/login`;
+			const emailInfo = [
+				{
+					to: email,
+					subject: 'Welcome to IKU Training!',
+					message: React.createElement(
+						'div',
+						null,
+						React.createElement(
+							'h2',
+							null,
+							`Welcome to IKU Training, ${firstName}!`
+						),
+						React.createElement(
+							'p',
+							null,
+							`Thank you for registering as a ${userRole} with IKU Training.`
+						),
+						React.createElement(
+							'p',
+							null,
+							'Your account has been successfully created and you can now access our training platform.'
+						),
+						React.createElement(
+							'p',
+							null,
+							'Click the link below to log in and start your training journey:'
+						),
+						React.createElement(
+							'a',
+							{
+								href: loginLink,
+								style: {
+									background: '#0066cc',
+									color: 'white',
+									padding: '10px 20px',
+									textDecoration: 'none',
+									borderRadius: '5px',
+									display: 'inline-block',
+									margin: '10px 0',
+								},
+							},
+							'Login to Your Account'
+						),
+						React.createElement(
+							'p',
+							null,
+							'If you have any questions, feel free to contact our support team.'
+						),
+						React.createElement('p', null, 'Welcome aboard!')
+					),
+				},
+			];
+
+			await sendEmails(emailInfo);
+		} catch (emailError) {
+			console.error('Failed to send welcome email:', emailError);
+			// Don't fail registration if email fails
+		}
+
 		return new NextResponse('User has been created', {
 			status: 201,
 		});

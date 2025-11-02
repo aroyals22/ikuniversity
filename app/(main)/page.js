9914bdca67d4d8864932db60b1241514d8bf70e0
Anthony Arroyo <anthony.aarroy2@gmail.com>
2025-11-02 10:54:25 -0600

    homepage hero details

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -20,8 +20,9 @@ const HomePage = async () => {
 
 	return (
 		<>
-			<section className='pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32 grainy'>
-				<div className='container flex max-w-[64rem] flex-col items-center gap-4 text-center relative isolate'>
+			<section className='pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32 grainy relative isolate'>
+				<div className='container flex max-w-[64rem] flex-col items-center gap-4 text-center relative'>
+					{/* Large impactful gradient blob */}
 					<div
 						aria-hidden='true'
 						className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
@@ -31,12 +32,10 @@ const HomePage = async () => {
 								clipPath:
 									'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
 							}}
-							className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#B92234] to-[#E63946] opacity-15 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+							className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[72rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#670c0c] via-[#1e3a8a] to-[#3b82f6] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[100rem]'
 						/>
 					</div>
-					{/* <span className='rounded-2xl bg-[#007AFF] text-white px-4 py-1.5 text-sm font-medium border shadow-lg'>
-						Welcome
-					</span> */}
+
 					<h1 className='font-heading text-3xl font-semibold sm:text-5xl md:text-6xl lg:text-7xl'>
 						Ikonix Training
 					</h1>
